ba98821e425be94cfb558dcf08d5c94d447e0996
Monica Saeed <monicasaeed12@gmail.com>
2023-05-14 16:27:51 +0300

    initial board with vertical and horizontal check for win

--- board.py
+++ board.py
@@ -21,3 +21,37 @@ class Board:
 
     def get_game_grid(self):
         return self.board
+
+    # place a piece in the column and return the row and column of the piece
+    def place_piece(self, column, player):
+        for i in range(len(self.board)-1, -1, -1):
+            row = self.board[i]
+            if row[column] == EMPTY:
+                row[column] = player
+                self.empty_spaces -= 1
+                return i, column
+        return -1, -1
+
+    # check if the board is full
+    def check_draw(self):
+        return (self.empty_spaces == 0)
+
+    # check if there are 4 in a row in any direction
+    def check_win(self, row, column, player):
+        return self._check_horizontal_win(row, column, player) or self._check_vertical_win(row, column, player) 
+
+    # go right and check if there are 4 right in a row
+    def _check_horizontal_win(self, row, column, player):
+        for i in range(column, column - 4, -1):
+            if (i > 3 or i < 0):
+                continue
+            r = self.board[row]
+            if (r[i] == player and r[i+1] == player and r[i+2] == player and r[i+3] == player):
+                return True
+        return False
+
+    # go down and check if there are 4 up in a row
+    def _check_vertical_win(self, row, column, player):
+        if (row < 3 and self.board[row+1][column] == player and self.board[row+2][column] == player and self.board[row+3][column] == player):
+            return True
+        return False
