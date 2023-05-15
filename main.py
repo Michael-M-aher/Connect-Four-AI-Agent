f61111c0e8ddcf4d3dbcee7e1beaab8fe1af46c1
MoayadR <moayad.r.abdalraouf@gmail.com>
2023-05-15 15:44:04 +0300

    + Main Game Loop

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from game import Game
+import time
 
 
 def main():
+    # print("Welcome to Connect 4!")
+    # print("Player 1 is Red")
+    # print("Player 2 is Blue")
+    # print("Player 1 goes first")
+    # print("To play, enter the column number you want to drop your piece in")
+    # print("The first player to get 4 in a row wins!")
+    # print("Good luck!")
+    # print("\n")
+    # print("Here is the board:")
+    # print(board.get_game_grid())
     game = Game()
     while not game.game_over:
-        # your code goes here
-        break
+        game.board.print_grid()
+        game.player1Turn()
+        if game.game_over:
+            break
+        game.board.print_grid()
+        game.player2Turn()
+    print("Game Over!")
 
 
 if __name__ == "__main__":
